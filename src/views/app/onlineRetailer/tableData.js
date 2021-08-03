bb3dfb1205416b0398ad777e8fcaa00112746e4b
kdh7565 <smrkdeogks@gmail.com>
2021-08-03 16:35:55 +0900

    product 내용 작업 중

---
+++ src/views/app/onlineRetailer/tableData.js
@@ -0,0 +1,249 @@
+/* eslint-disable import/prefer-default-export */
+export const Columns = [
+  {
+    Header: 'Channel',
+    accessor: 'channel',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/1',
+    accessor: 'count1',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/2',
+    accessor: 'count2',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/3',
+    accessor: 'count3',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/4',
+    accessor: 'count4',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/5',
+    accessor: 'count5',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/6',
+    accessor: 'count6',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/7',
+    accessor: 'count7',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/8',
+    accessor: 'count8',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/9',
+    accessor: 'count9',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/10',
+    accessor: 'count10',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/11',
+    accessor: 'count11',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/12',
+    accessor: 'count12',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/13',
+    accessor: 'count13',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/14',
+    accessor: 'count14',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/15',
+    accessor: 'count15',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/16',
+    accessor: 'count16',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/17',
+    accessor: 'count17',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/18',
+    accessor: 'count18',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/19',
+    accessor: 'count19',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/20',
+    accessor: 'count20',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/21',
+    accessor: 'count21',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+  {
+    Header: '4/22',
+    accessor: 'count22',
+    cellClass: 'text-center',
+    disableSortBy: true
+  },
+];
+
+export const TableData = [
+  {
+    id: 1,
+    channel: 'Coupang',
+    count1: 29,
+    count2: 12,
+    count3: 65,
+    count4: 23,
+    count5: 10,
+    count6: 65,
+    count7: 35,
+    count8: 76,
+    count9: 55,
+    count10: 55,
+    count11: 55,
+    count12: 55,
+    count13: 55,
+    count14: 29,
+    count15: 12,
+    count16: 65,
+    count17: 23,
+    count18: 10,
+    count19: 65,
+    count20: 35,
+    count21: 76,
+    count22: 55,
+  },
+  {
+    id: 2,
+    channel: '11st',
+    count1: 29,
+    count2: 12,
+    count3: 65,
+    count4: 23,
+    count5: 10,
+    count6: 65,
+    count7: 35,
+    count8: 76,
+    count9: 55,
+    count10: 55,
+    count11: 55,
+    count12: 55,
+    count13: 55,
+    count14: 29,
+    count15: 12,
+    count16: 65,
+    count17: 23,
+    count18: 10,
+    count19: 65,
+    count20: 35,
+    count21: 76,
+    count22: 55,
+  },
+  {
+    id: 3,
+    channel: 'Gmarket',
+    count1: 29,
+    count2: 12,
+    count3: 65,
+    count4: 23,
+    count5: 10,
+    count6: 65,
+    count7: 35,
+    count8: 76,
+    count9: 55,
+    count10: 55,
+    count11: 55,
+    count12: 55,
+    count13: 55,
+    count14: 29,
+    count15: 12,
+    count16: 65,
+    count17: 23,
+    count18: 10,
+    count19: 65,
+    count20: 35,
+    count21: 76,
+    count22: 55,
+  },
+  {
+    id: 4,
+    channel: 'Timon',
+    count1: 29,
+    count2: 12,
+    count3: 65,
+    count4: 23,
+    count5: 10,
+    count6: 65,
+    count7: 35,
+    count8: 76,
+    count9: 55,
+    count10: 55,
+    count11: 55,
+    count12: 55,
+    count13: 55,
+    count14: 29,
+    count15: 12,
+    count16: 65,
+    count17: 23,
+    count18: 10,
+    count19: 65,
+    count20: 35,
+    count21: 76,
+    count22: 55,
+  },
+];
+
